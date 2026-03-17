52225434d45186dce193f2c946fdd8f91db4529b
TobiaKW <kevinwong391@gmail.com>
2026-03-18 00:26:43 +0800

    deployment fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,9 @@ set -o errexit
 bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
+
+# Run migrations instead of full schema load to avoid timeouts if DB is large
+# but for first time, we use schema:load
+# bin/rails db:migrate
 bin/rails db:schema:load
-bin/rails db:seed 
+bin/rails db:seed
