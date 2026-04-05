4f9bfe3b7037465e3a3b62667b8a8db3cc791301
TobiaKW <kevinwong391@gmail.com>
2026-04-05 14:53:57 +0800

    importmap fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,3 @@ pin "application"
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.1.200
 pin_all_from "app/javascript/channels", under: "channels"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "chartkick", to: "chartkick.umd.js"
-pin "chart.js", to: "chart.min.js"
