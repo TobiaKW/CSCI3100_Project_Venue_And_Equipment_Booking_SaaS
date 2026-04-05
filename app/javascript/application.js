74263cbc95b4a2d44f4165a9aca71067199b5bd1
TobiaKW <kevinwong391@gmail.com>
2026-04-05 12:03:35 +0800

    import chartkick to js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "channels"
 import "@hotwired/turbo-rails"
+import "chartkick"
+import "chart.js"
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,5 @@ pin "application"
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.1.200
 pin_all_from "app/javascript/channels", under: "channels"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "chartkick", to: "chartkick.umd.js"
+pin "chart.js", to: "chart.min.js"
