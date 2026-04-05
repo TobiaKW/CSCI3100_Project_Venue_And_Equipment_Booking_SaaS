5b36716906eacd8a78011d50ddad4f778c824224
TobiaKW <kevinwong391@gmail.com>
2026-04-05 11:20:22 +0800

    deployment fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,6 @@ set -o errexit
 bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
-bin/rails db:migrate
-# bin/rails db:schema:load
-bin/rails db:seed
+
+# Reset database on every deploy
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:reset_prod
