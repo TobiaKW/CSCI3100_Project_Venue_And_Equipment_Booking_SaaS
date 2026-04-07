f05498c1a2cd0e813629a7320d85ff095efe7ce6
abcd <abcd@gmail.com>
2026-04-07 19:50:41 +0800

    Fix the bug of username could be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,17 @@ class User < ApplicationRecord
 
   belongs_to :department
   has_many :bookings, dependent: :restrict_with_error
-
+  before_validation :normalize_name
+  validates :name, presence: true
   before_validation :set_default_role, on: :create
 
   private
 
   def set_default_role
     self.role ||= "student"
+  end
+
+  def normalize_name
+    self.name = name.to_s.strip.presence
   end
 end
